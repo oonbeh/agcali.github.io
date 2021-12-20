e97f48af339f0743b3b1455b55217d898d6f5fd2
oonbeh <oonbeh@gmail.com>
2021-12-20 11:06:19 +0800

    Add files via upload

---
+++ ads728.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '03ec2f09fb292db9f4dffc74cf93f627',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/03ec2f09fb292db9f4dffc74cf93f627/invoke.js"></scr' + 'ipt>');
