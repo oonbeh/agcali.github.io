37581896cfb934bdc7064522c5b9ac28edf770ad
oonbeh <oonbeh@gmail.com>
2022-03-02 00:57:17 +0800

    Update ads728.js

--- ads728.js
+++ ads728.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '03ec2f09fb292db9f4dffc74cf93f627',
+		'key' : '9795378f3bef1d423a41e1b70bc5341b',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.variousformatscontent.com/03ec2f09fb292db9f4dffc74cf93f627/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/9795378f3bef1d423a41e1b70bc5341b/invoke.js"></scr' + 'ipt>');
