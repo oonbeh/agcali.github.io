8ef4219adae2023090ca5b638f2eb3f3d259a89e
oonbeh <oonbeh@gmail.com>
2022-03-02 01:03:02 +0800

    Add files via upload

---
+++ adsbanner.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '9795378f3bef1d423a41e1b70bc5341b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/9795378f3bef1d423a41e1b70bc5341b/invoke.js"></scr' + 'ipt>');
