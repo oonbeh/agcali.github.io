151d8ecd905b5fc97165f2346efbda9f0c6ddf5a
oonbeh <oonbeh@gmail.com>
2022-03-02 14:24:42 +0800

    Add files via upload

---
+++ adsiklan.js
@@ -0,0 +1,12 @@
+//---- social bar adstera belajar.odong.my.id --
+(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//merelyerroneous.com/c7/73/89/c77389b8ef57a50f03e3a43b57a8205a.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
+//---- POPUNDER adstera apailmu.my.id --
+// (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//merelyerroneous.com/89/c8/53/89c8536358cc34eb325bd501d77fb86f.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
+//---- BANNER adstera zonailmu.my.id --
+(function(){injectScript([{"attr":[{"name":"src","value":"//agcali.pahe.my.id/adsbanner.js"},{"name":"type","value":"text/javascript"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
+//---- POPUP Propulerads zonailmu.my.id --
+(function(){injectScript([{"attr":[],"tag":"script","inner":"(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4905422,document.body||document.documentElement)"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
