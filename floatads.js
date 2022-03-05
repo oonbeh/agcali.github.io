16fa492e8130271ef5b6d6a7e37f76105232f1ec
oonbeh <oonbeh@gmail.com>
2022-03-06 00:43:49 +0800

    Create floatads.js

---
+++ floatads.js
@@ -0,0 +1,41 @@
+<!-- floating ads  -->
+<script type='text/javascript'>
+$(document).ready(function() {
+$('img#closed').click(function(){
+$('#btm_banner').hide(90);
+});
+});
+</script>
+<style type='text/css'>
+div#btm_banner {
+bottom: 0;
+position: fixed;
+width: 100%;
+opacity: 0.9;
+left: 0;
+}
+div#btm_banner img{
+border:0;
+cursor:pointer;
+}
+</style>
+<div style='height: 0px;'></div>
+<div align='center' id='btm_banner' style='height: 120px; z-index: 9999;'>
+<div style='text-align: right; width: 728px; height: 10px;'>
+<img id='closed' src='http://4.bp.blogspot.com/-9MWyoN5VsJM/TivTpPyUuhI/AAAAAAAABL0/ldO739MTRBg/s1600/close3.png'/></div>
+<div style='clear: both;'></div>
+<p><p><center>
+
+	atOptions = {
+		'key' : '9795378f3bef1d423a41e1b70bc5341b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/9795378f3bef1d423a41e1b70bc5341b/invoke.js"></scr' + 'ipt>');
+
+
+</center></p>
+</p>
+</div>
