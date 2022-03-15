bcd1c930496be68f3f71871555d1e1c68870a416
oonbeh <oonbeh@gmail.com>
2022-03-16 00:28:48 +0800

    Update adsiklan.js

--- adsiklan.js
+++ adsiklan.js
@@ -5,14 +5,6 @@
 // (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//merelyerroneous.com/89/c8/53/89c8536358cc34eb325bd501d77fb86f.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
 //---- BANNER adstera zonailmu.my.id --
-	atOptions = {
-		'key' : '9795378f3bef1d423a41e1b70bc5341b',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highconvertingformats.com/9795378f3bef1d423a41e1b70bc5341b/invoke.js"></scr' + 'ipt>');
 
 //---- POPUP Propulerads zonailmu.my.id --
 // (function(){injectScript([{"attr":[],"tag":"script","inner":"(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4905422,document.body||document.documentElement)"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
