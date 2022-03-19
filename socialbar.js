166427329b676e6cf135ee02ca38f84485523e1f
oonbeh <oonbeh@gmail.com>
2022-03-19 12:03:12 +0800

    Update socialbar.js

--- socialbar.js
+++ socialbar.js
@@ -1,1 +1,5 @@
+// ---- SocialBar
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//evaluationgnatprotected.com/a0/96/6f/a0966fc4f1d2fa783f67aa6c5dd2fca1.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
+// -----  Popup
+(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//evaluationgnatprotected.com/a9/61/25/a96125cbd3346eedd2864b8c4d5169b9.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
