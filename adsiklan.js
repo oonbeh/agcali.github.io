f8cccef4334996582a32b0769934fa1c4192f361
oonbeh <oonbeh@gmail.com>
2022-03-20 18:30:40 +0800

    Update adsiklan.js

--- adsiklan.js
+++ adsiklan.js
@@ -4,10 +4,21 @@
 //---- POPUNDER adstera apailmu.my.id --
 // (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//merelyerroneous.com/89/c8/53/89c8536358cc34eb325bd501d77fb86f.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
-//---- BANNER adstera zonailmu.my.id --
+//---- Sosial-Barar adstera zonailmu.my.id --
+(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//evaluationgnatprotected.com/4a/00/1e/4a001e886418d57f6154e262bac9b581.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
 //---- POPUP Propulerads zonailmu.my.id --
 // (function(){injectScript([{"attr":[],"tag":"script","inner":"(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4905422,document.body||document.documentElement)"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
 // ---- Natip Adstera
-(function(){injectScript([{"attr":[{"name":"async","value":"async"},{"name":"data-cfasync","value":"false"},{"name":"src","value":"//evaluationgnatprotected.com/70e7aea2cc1ff53dd1bce954323be114/invoke.js"}],"tag":"script","inner":""},{"attr":[{"name":"id","value":"container-70e7aea2cc1ff53dd1bce954323be114"}],"tag":"div","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+// (function(){injectScript([{"attr":[{"name":"async","value":"async"},{"name":"data-cfasync","value":"false"},{"name":"src","value":"//evaluationgnatprotected.com/70e7aea2cc1ff53dd1bce954323be114/invoke.js"}],"tag":"script","inner":""},{"attr":[{"name":"id","value":"container-70e7aea2cc1ff53dd1bce954323be114"}],"tag":"div","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
+// --- Banner 468 * 60 zonailmu.my.id
+	atOptions = {
+		'key' : 'aa9ce4532d106ddcffe05d04c09dfb16',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://evaluationgnatprotected.com/aa9ce4532d106ddcffe05d04c09dfb16/invoke.js"></scr' + 'ipt>');
