38bf670cd3d252eceecf91bd8f07713fa6a7bbb4
oonbeh <oonbeh@gmail.com>
2022-06-04 05:39:20 +0800

    Update adsiklan.js

--- adsiklan.js
+++ adsiklan.js
@@ -1,26 +1,11 @@
-//---- social bar adstera belajar.odong.my.id --
-//(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//merelyerroneous.com/c7/73/89/c77389b8ef57a50f03e3a43b57a8205a.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
-
 // ---- Shopee Directlink
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"\n//<![CDATA[\n//--- setting ---------\nvar refererWeb=[\n  \"www.google.com\",\n  \"www.bing.com\",\n  \"yandex.com\",\n  \"yahoo.com\",\n  \".pinterest.\"\n];\nvar directTo=\"https://shope.ee/6Ui1vEpp3J\";\n\n\n//--- main script ---------\nif(document.referrer){\n  var aaRef = document.referrer;\n  refererWeb.forEach(function(a){\n   if(aaRef.indexOf(a)>=0){\n\twindow.location.href=directTo;\n   };\n  });\n};\n//]]>\n"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
 // ---- PopupShope
 (function(){injectScript([{"attr":[{"name":"src","value":"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"},{"name":"integrity","value":"sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ="},{"name":"crossorigin","value":"anonymous"}],"tag":"script","inner":""},{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"\n\t$(document).ready(function()\n\t{\n\t  $('body').addClass('xepo_ads');\n\t});\n\n\t$(document).on('click','.xepo_ads',function(e)\n\t{\n\t    $(this).removeClass('xepo_ads');\n\n\t    //ubah google dengan ads direct link\n\t    window.open('https://shope.ee/6Ui1vEpp3J', '_blank');\n\t});\n"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
-//--- Sosial Bar Apailmu.my.id
-//(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//evaluationgnatprotected.com/5a/ca/3c/5aca3c96a1023b796a09aada11d0734b.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
-
 //---- POPUNDER adstera apailmu.my.id --
 // (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//merelyerroneous.com/89/c8/53/89c8536358cc34eb325bd501d77fb86f.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
 //---- Sosial-Barar adstera zonailmu.my.id --
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//evaluationgnatprotected.com/4a/00/1e/4a001e886418d57f6154e262bac9b581.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
-
-//---- POPUP Propulerads zonailmu.my.id --
-// (function(){injectScript([{"attr":[],"tag":"script","inner":"(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4905422,document.body||document.documentElement)"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
-
-// ---- Natip Adstera
-// (function(){injectScript([{"attr":[{"name":"async","value":"async"},{"name":"data-cfasync","value":"false"},{"name":"src","value":"//evaluationgnatprotected.com/70e7aea2cc1ff53dd1bce954323be114/invoke.js"}],"tag":"script","inner":""},{"attr":[{"name":"id","value":"container-70e7aea2cc1ff53dd1bce954323be114"}],"tag":"div","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
-
-// --- Banner 468 * 60 zonailmu.my.id
-	
