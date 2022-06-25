1a0345b11a65a341c1100d94eded596478c2199a
oonbeh <oonbeh@gmail.com>
2022-06-25 15:53:01 +0800

    Update adsiklan.js

--- adsiklan.js
+++ adsiklan.js
@@ -14,7 +14,7 @@ const mapAds = [
 	"/analytic.js",
 	"/anti-adblock.js",
 	"/disable-costom-button-back.js",
-	"shopefull.js",
+	"/shopefull.js",
 	"/banner.js"
 ];
 var hostUri = "odong.eduzone.my.id";
